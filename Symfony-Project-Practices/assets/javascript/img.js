680af76f9609dc330a1ad52beea69757014a6813
Jaber Khan <md.jaberkhan66@gmail.com>
2026-04-06 00:00:20 +0600

    symfony-project-practice

---
+++ Symfony-Project-Practices/assets/javascript/img.js
@@ -0,0 +1,5 @@
+import logo from "./images/logo.png";
+
+const img = document.createElement("img");
+img.src = logo;
+document.body.appendChild(img);
--- Symfony-Project-Practices/src/Controller/PageController.php
+++ Symfony-Project-Practices/src/Controller/PageController.php
@@ -1,15 +1,49 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class PageController
+class PageController extends AbstractController
 {
-    #[Route("/page", name: "app_page")]
+    #[Route("/home", name: "app_home")]
     public function index(): Response
     {
-        return new Response("Welcome to the Page Controller!");
+        $randomText = "Welcome to the Symfony World";
+        return $this->render("page/index.html.twig", [
+            "controller_name" => "HomeController",
+            "title" => $randomText,
+        ]);
+    }
+
+    #[Route("/about", name: "app_about")]
+    public function about(): Response
+    {
+        $title = "About Us";
+        return $this->render("page/about.html.twig", [
+            "title" => $title,
+        ]);
+    }
+
+    #[Route("/contact", name: "app_contact")]
+    public function contact(): Response
+    {
+        $title = "Contact Us";
+        return $this->render("page/contact.html.twig", [
+            "title" => $title,
+        ]);
+    }
+    # define named routes
+    #[Route("/items", name: "page_items")]
+    public function list(): Response
+    {
+        $title = "List of Items";
+        $items = ["Item 1", "Item 2", "Item 3"];
+        return $this->render("page/list.html.twig", [
+            "title" => $title,
+            "items" => $items,
+        ]);
     }
 
     #[Route("/page/{slug}", name: "app_page_show")]
